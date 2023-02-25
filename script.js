4bb210fcfa1e64a20ec9a607e8751a7b26fc2f60
CAG2Mark <markng989@gmail.com>
2023-02-25 16:59:03 +0800

    Copy button

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+var tokenArea = document.getElementById("token-area");
+
 let token = window.location.hash.split("=")[1].split("&")[0];
-document.getElementById("token-area").innerHTML = token;
+
+let button = document.getElementById("copy-button");
+
+document.getElementById("copy-button").addEventListener("click", (e) => {
+    navigator.clipboard.writeText(token);
+    button.innerHTML = "Copied!";
+})
