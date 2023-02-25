a6bfb5ad0d5310402a369c7539d3f8c52db361b1
CAG2Mark <markng989@gmail.com>
2023-02-25 20:01:25 +0800

    styling

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let token = window.location.hash.split("=")[1].split("&")[0];
 
 let button = document.getElementById("copy-button");
 
-document.getElementById("copy-button").addEventListener("click", (e) => {
+button.addEventListener("click", (e) => {
     navigator.clipboard.writeText(token);
     button.innerHTML = "Copied!";
+
+    button.style.width = "120px";
 })
+
